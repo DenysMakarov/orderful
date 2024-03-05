d27775fe2e1c8dbdc3197e4a3d340905f72530cb
Denys Makarov <denys.makarow@gmail.com>
2024-03-05 10:45:57 -0700

    commites were removed

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,12 +2,10 @@ export interface EDIJsonObject {
     [key: string]: Array<{ [key: string]: string | string[] }>;
 }
 
-// Define the structure of each property within 'root'
 export interface RootProperty {
     [key: string]: string | string[];
 }
 
-// Define the structure of the 'root' object, which contains dynamically named properties
 export interface ParsedXmlRoot {
     [key: string]: RootProperty | RootProperty[];
 }
